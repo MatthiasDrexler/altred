185548c520a1df226e45f6a6017e8e6468751630
Matthias Drexler <matthias.drexler@andrena.de>
2021-09-19 11:07:43 +0000

    Add basic unit test for sign up endpoint

--- microservices/user-management/backend/src/main.rs
+++ microservices/user-management/backend/src/main.rs
@@ -1,13 +1,13 @@
 mod controller;
 
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(controller::signup_controller::sign_up)
+        App::new().service(web::resource("/").to(controller::signup_controller::sign_up))
     })
     .bind("127.0.0.1:8080")?
     .run()
-    .await}
+    .await
+}
