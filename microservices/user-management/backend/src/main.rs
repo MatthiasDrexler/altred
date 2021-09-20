e0ea30c02660a75a972318f4dcf9db5f32211d0e
Matthias Drexler <matthias.drexler@andrena.de>
2021-09-21 00:52:25 +0200

    Read port from environment variable

--- microservices/user-management/backend/src/main.rs
+++ microservices/user-management/backend/src/main.rs
@@ -1,11 +1,21 @@
 mod controller;
 
 use actix_web::{web, App, HttpServer};
+use std::env;
+
+const ACCEPT_REQUESTS_FROM: &str = "0.0.0.0";
+const PORT_ENVIRONMENT_VARIABLE_KEY: &str = "PORT";
+const DEFAULT_PORT: &str = "8080";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::resource("/").to(controller::sign_up::sign_up)))
-        .bind("127.0.0.1:8080")?
+    HttpServer::new(|| App::new().service(web::resource("/signup").to(controller::sign_up::sign_up)))
+        .bind(bind_to())?
         .run()
         .await
+}
+
+fn bind_to() -> String {
+    let at_port = env::var(PORT_ENVIRONMENT_VARIABLE_KEY).unwrap_or(String::from(DEFAULT_PORT));
+    format!("{}:{}", ACCEPT_REQUESTS_FROM, at_port)
 }
