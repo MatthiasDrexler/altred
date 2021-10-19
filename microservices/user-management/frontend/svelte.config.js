9612b7ddb0bf7340416515beffefff479e64e25b
Matthias Drexler <matthias.drexler@andrena.de>
2021-10-19 21:58:00 +0200

    Remove carbon preprocessor
    Signed-off-by: Matthias Drexler <matthias.drexler@andrena.de>

--- microservices/user-management/frontend/svelte.config.js
+++ microservices/user-management/frontend/svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from "@sveltejs/adapter-node"
-import { optimizeImports } from "carbon-preprocess-svelte"
 import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [optimizeImports(), preprocess()],
+  preprocess: [preprocess()],
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
