84ac4e40c1f65d8882750ac4226f07ca005a2a46
Matthias Drexler <matthias.drexler@andrena.de>
2021-10-20 22:29:15 +0200

    Use dotenv instead of compile time replacement by vite

--- microservices/user-management/frontend/src/lib/domain/configuration/names.ts
+++ microservices/user-management/frontend/src/lib/domain/configuration/names.ts
@@ -1,4 +1,4 @@
 export class Names {
-  public static readonly SUITE_NAME: string = import.meta.env.ALTRED_UM_SUITE_NAME.toString()
-  public static readonly APPLICATION_NAME: string = import.meta.env.ALTRED_UM_APPLICATION_NAME.toString()
+  public static readonly SUITE_NAME: string = process.env["ALTRED_UM_SUITE_NAME"]
+  public static readonly APPLICATION_NAME: string = process.env["ALTRED_UM_APPLICATION_NAME"]
 }
