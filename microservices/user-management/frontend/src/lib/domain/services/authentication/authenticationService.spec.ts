363c9cd3b11c6539372dc96eb4f9b52fb69ff28e
Matthias Drexler <matthias.drexler@andrena.de>
2021-10-28 20:28:15 +0200

    Move fetch of token to service

--- microservices/user-management/frontend/src/lib/domain/services/authentication/authenticationService.spec.ts
+++
@@ -1,7 +0,0 @@
-import { authenticationService } from "./authenticationService"
-
-describe("AuthenticationService", () => {
-  it("should be initially unauthenticated", () => {
-    expect(authenticationService.isAuthenticated()).toBeFalsy()
-  })
-})
--- microservices/user-management/frontend/src/routes/token.ts
+++ microservices/user-management/frontend/src/routes/token.ts
@@ -1,5 +1,4 @@
-import { auth } from "$lib/domain/configuration/auth"
-import { AccessTokenDto } from "$lib/dtos/accessTokenDto"
+import { AuthorizationService } from "$lib/domain/services/authorization/authorizationService"
 import type { ServerRequest } from "@sveltejs/kit/types/hooks"
 
 type EndpointOutput = { body }
@@ -11,37 +10,8 @@ export const post = async (request: ServerRequest): Promise<EndpointOutput> => {
   console.log(code)
   console.log(sessionState)
 
-  const accessToken = await retrieveTokenForUser(code)
+  const accessToken = await new AuthorizationService().retrieveTokenForUser(code)
   if (accessToken) {
     return { body: accessToken }
-  }
-}
-
-const retrieveTokenForUser = async (authorizationCode: string): Promise<AccessTokenDto> => {
-  try {
-    const response = await fetch(auth.AUTH_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-      },
-      body: new URLSearchParams({
-        grant_type: "authorization_code",
-        client_id: auth.AUTH_CLIENT_ID,
-        client_secret: auth.AUTH_CLIENT_SECRET,
-        code: authorizationCode,
-        redirect_uri: auth.AUTH_REDIRECT_URI,
-      }),
-    })
-
-    if (!response.ok) {
-      console.log(await response.text())
-      return undefined
-    }
-
-    const responseJson = await response.json()
-
-    return new AccessTokenDto(responseJson.token_type, responseJson.access_token)
-  } catch (error: unknown) {
-    console.error("Could not retrieve token: " + error)
   }
 }
